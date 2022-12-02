a088124843fcaa975e8b82b4cf05a2c6ab738450
Jason Spadaro <jpspadaro@gmail.com>
2022-12-02 16:21:27 -0500

    Starting to define private functions for bath processing.

--- src/batchtools.rs
+++ src/batchtools.rs
@@ -1,7 +1,24 @@
+use std::fmt::Error;
 use image::{GenericImage, GenericImageView, ImageBuffer, DynamicImage, imageops};
 use rayon::prelude::*;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
+
+fn open_image(file: String, verbose: bool) -> DynamicImage {
+    //TODO: open image
+}
+
+fn write_image(file_out: String, verbose: bool) {
+
+}
+
+fn create_cache_images (file_vec: Vec<String>, verbose: bool) -> Result<u32, Error>{
+    //TODO: make cache images for internal use
+}
+
+fn remove_cache_images (verbose: bool) -> Result<u32, Error> {
+    //TODO: Remove cache images
+}
 
 pub fn get_file_vector_from_batch (batch_file: String, verbose: bool) -> io::Result<Vec<String>> {
     //Makes a vector of strings from a list in a file.
